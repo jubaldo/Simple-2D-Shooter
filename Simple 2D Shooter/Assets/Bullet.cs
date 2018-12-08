47ebc1f7f462929eca166835347a99e02a12684d
jubaldo <darknaruto.jr117@gmail.com>
2018-12-07 21:20:15 -0800

    First Commit
    Uploading Unity Project to Github

---
+++ Simple 2D Shooter/Assets/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+    public int damage = 20;
+    public float speed = 20f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+
+    }
+}
---
+++ Simple 2D Shooter/Assets/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    public Transform firepoint;
+    public GameObject bulletPre;
+    
+	// Use this for initialization
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+	}
+	
+	// Update is called once per frame
+	void Shoot () {
+        Instantiate(bulletPre, firepoint.position, firepoint.rotation);
+	}
+}
